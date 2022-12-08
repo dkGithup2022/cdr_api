c6f28450f7a49e3a16db525abc390b92574f7b74
dkGithup2022 <104286091+dkGithup2022@users.noreply.github.com>
2022-12-08 22:53:43 +0900

    Test: ParamerizedTest 공통부분 클래스로

--- cdr_api_app/src/test/java/com/dk0124/cdr/cdrApi/controller/CdrApiValidatorTest.java
+++ cdr_api_app/src/test/java/com/dk0124/cdr/cdrApi/controller/CdrApiValidatorTest.java
@@ -1,6 +1,7 @@
 package com.dk0124.cdr.cdrApi.controller;
 
 import com.dk0124.cdr.cdrApi.exception.InvalidApiParameterException;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -22,20 +23,11 @@ class CdrApiValidatorTest {
                                      String vendorCode,
                                      String coinCode
     ) {
-        //given
-        //when & then
         apiValidator.checkPathParams(dataType, vendorCode, coinCode);
     }
 
     private static Stream<Arguments> provide_valid_api_path_parameters() { // argument source method
-        return Stream.of(
-                Arguments.of("ticks", "upbit", "KRW-BTC"),
-                Arguments.of("orderbooks", "upbit", "KRW-BTC"),
-                Arguments.of("candles", "upbit", "KRW-BTC"),
-                Arguments.of("ticks", "bithumb", "BTC_KRW"),
-                Arguments.of("orderbooks", "bithumb", "BTC_KRW"),
-                Arguments.of("candles", "bithumb", "BTC_KRW")
-        );
+        return ParameterSupplier.getValidPathParams();
     }
 
 
@@ -46,8 +38,6 @@ class CdrApiValidatorTest {
                                   String vendorCode,
                                   String coinCode
     ) {
-        //given
-        //when & then
         assertThrows(InvalidApiParameterException.class, () -> {
             apiValidator.checkPathParams(dataType, vendorCode, coinCode);
         });
@@ -55,23 +45,27 @@ class CdrApiValidatorTest {
     }
 
     private static Stream<Arguments> provide_bad_api_path_parameters() { // argument source method
-        return Stream.of(
-                Arguments.of("", "upbit", "KRW-BTC"),
-                Arguments.of("orderbooks", "", "KRW-BTC"),
-                Arguments.of("candles", "upbit", ""),
-                Arguments.of("ticks", "bithumb", "xxx"),
-                Arguments.of("orderbooks", "xxx", "BTC_KRW"),
-                Arguments.of("xxx", "bithumb", "BTC_KRW")
-        );
+        return ParameterSupplier.getInvalidPathParams();
     }
+
+    @DisplayName("TEST :CdrApiValidator.checkRequestParams(timestamp, size, page) 좋은 입력에 대해 테스트 ")
+    @ParameterizedTest(name = "index ={index}, dataType={0}, vendorCode={1}, coinCode={2} ")
+    @MethodSource("provide_valid_api_request_parameters")
+    void validate_request_param_success(Long timestamp, Integer size, Integer page
+    ) {
+        apiValidator.checkRequestParams(timestamp, size, page);
+    }
+
+    private static Stream<Arguments> provide_valid_api_request_parameters() { // argument source method
+        return ParameterSupplier.getValidQueryParams();
+    }
+
 
     @DisplayName("TEST :CdrApiValidator.checkRequestParams(timestamp, size, page) 나쁜 입력에 대해 InvalidApiParameterException ")
     @ParameterizedTest(name = "index ={index}, dataType={0}, vendorCode={1}, coinCode={2} ")
     @MethodSource("provide_bad_api_request_parameters")
     void validate_request_param_fail(Long timestamp, Integer size, Integer page
     ) {
-        //given
-        //when & then
         assertThrows(InvalidApiParameterException.class, () -> {
             apiValidator.checkRequestParams(timestamp, size, page);
         });
@@ -80,32 +74,8 @@ class CdrApiValidatorTest {
     }
 
     private static Stream<Arguments> provide_bad_api_request_parameters() { // argument source method
-        return Stream.of(
-                Arguments.of(1670252400000L - 10, 200, 0),
-                Arguments.of(null, 200, 0),
-                Arguments.of(System.currentTimeMillis() + 100, 2000 + 1, 100),
-                Arguments.of(System.currentTimeMillis() + 100, 200, -1)
-        );
+        return ParameterSupplier.getInValidQueryParams();
     }
 
-    @DisplayName("TEST :CdrApiValidator.checkRequestParams(timestamp, size, page) 좋은 입력에 대해 테스트 ")
-    @ParameterizedTest(name = "index ={index}, dataType={0}, vendorCode={1}, coinCode={2} ")
-    @MethodSource("provide_valid_api_request_parameters")
-    void validate_request_param_success(Long timestamp, Integer size, Integer page
-    ) {
-        //given
-        //when & then
-        apiValidator.checkRequestParams(timestamp, size, page);
-    }
 
-    private static Stream<Arguments> provide_valid_api_request_parameters() { // argument source method
-        return Stream.of(
-                Arguments.of(1670252400000L + 10, 200, 0),
-                Arguments.of(1670252400000L, 200, 0),
-                Arguments.of(System.currentTimeMillis() + 100, 200, 100),
-                Arguments.of(System.currentTimeMillis() + 100, 200, 200),
-                Arguments.of(System.currentTimeMillis(), 200, 2000)
-
-        );
-    }
 }
---
+++ cdr_api_app/src/test/java/com/dk0124/cdr/cdrApi/controller/ParameterSupplier.java
@@ -0,0 +1,50 @@
+package com.dk0124.cdr.cdrApi.controller;
+
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.util.stream.Stream;
+
+public class ParameterSupplier {
+    public static Stream<Arguments> getInValidQueryParams() { // argument source method
+        return Stream.of(
+                Arguments.of(1670252400000L - 10, 200, 0),
+                Arguments.of(null, 200, 0),
+                Arguments.of(System.currentTimeMillis() + 100, 2000 + 1, 100),
+                Arguments.of(System.currentTimeMillis() + 100, 200, -1)
+        );
+
+    }
+
+    public static Stream<Arguments> getValidQueryParams() { // argument source method
+        return Stream.of(
+                Arguments.of(1670252400000L + 10, 200, 0),
+                Arguments.of(1670252400000L, 200, 0),
+                Arguments.of(System.currentTimeMillis() + 100, 200, 100),
+                Arguments.of(System.currentTimeMillis() + 100, 200, 200),
+                Arguments.of(System.currentTimeMillis(), 200, 2000)
+        );
+    }
+
+    public static Stream<Arguments> getInvalidPathParams(){
+        return Stream.of(
+                Arguments.of("", "upbit", "KRW-BTC"),
+                Arguments.of("orderbooks", "", "KRW-BTC"),
+                Arguments.of("candles", "upbit", ""),
+                Arguments.of("ticks", "bithumb", "xxx"),
+                Arguments.of("orderbooks", "xxx", "BTC_KRW"),
+                Arguments.of("xxx", "bithumb", "BTC_KRW")
+        );
+    }
+
+    public static Stream<Arguments> getValidPathParams(){
+        return Stream.of(
+                Arguments.of("ticks", "upbit", "KRW-BTC"),
+                Arguments.of("orderbooks", "upbit", "KRW-BTC"),
+                Arguments.of("candles", "upbit", "KRW-BTC"),
+                Arguments.of("ticks", "bithumb", "BTC_KRW"),
+                Arguments.of("orderbooks", "bithumb", "BTC_KRW"),
+                Arguments.of("candles", "bithumb", "BTC_KRW")
+        );
+    }
+
+}
